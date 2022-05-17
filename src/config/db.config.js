1c2c0bbf1635707ed8385b6a03d0ec8f28b280dc
aldaine-AG <aldaineclarke1@gmail.com>
2022-05-16 21:49:16 -0500

    initial file structure commit

---
+++ src/config/db.config.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const db  = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+
+
+});
+
+// Connects to the database with the credentials provided.
+db.connect((error)=>{
+    if(error) throw error;
+    console.log("database connection established");
+});
+
+
+module.exports = db;
---
+++ src/services/db.service.js
@@ -0,0 +1,12 @@
+const db = require("../config/db.config")
+
+// This page has all the queries for the database.
+
+
+
+
+
+
+module.exports = {
+
+}
