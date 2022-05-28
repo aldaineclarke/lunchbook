e27d861fddae0f34acfa705c9ac3278f49cb8123
aldaine-AG <aldaineclarke1@gmail.com>
2022-05-27 20:00:19 -0500

    implemented the session and created routes for admin login

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,17 +1,36 @@
+const db = require("../config/db.config");
 
 function getLoginPage(req, res, next){
-    res.render("login")
+    res.render("login",{messages:{error: req.flash("error")}})
 }
-// function getOrderPage(req, res, next){
-//     res.render("MealOrder")
-// }
+function authenticateUser(req, res,next){
+    let password = req.body.password;
+    let email = req.body.email;
+    console.log({password, email})
+
+    db.query(`SELECT * FROM administrators WHERE email = '${req.body.email}' `, email, (error, records, fields)=>{
+        if(error) throw error;
+        if(records.length < 1){
+            req.flash("error", "The credentials were incorrect.");
+            return res.redirect("/login")
+        }
+        let user = (records[0].password == password) ? records[0] : null;
+        
+        if(user){
+            req.session.user = user;
+            res.redirect(200,"/")
+        }
+    })
+}
+
 function getHomePage(req, res, next){
-    res.render("MealOrder")
+    res.render("MealOrder",{user: req.session.user})
 }
 
 
 module.exports = {
     getLoginPage,
     getHomePage,
+    authenticateUser
 
 }
